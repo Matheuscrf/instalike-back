c51b45b7e9f437aff2a56efb78cf3c4c796f4119
Matheus Teixeira <149537799+Matheuscrf@users.noreply.github.com>
2024-11-23 23:25:06 -0300

    Implementando armazenamento e Upload de imagens

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -5,11 +5,17 @@ import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para obter todos os posts do banco de dados.
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
   // Seleciona o banco de dados "imersao-instabytes".
   const db = conexao.db("imersao-instabytes");
   // Seleciona a coleção "posts" dentro do banco de dados.
   const colecao = db.collection("posts");
   // Executa uma consulta para encontrar todos os documentos (posts) na coleção e retorna os resultados como um array.
   return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-instabytes");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,37 @@
+// Framework para criar servidores web
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+// Middleware para lidar com uploads de arquivos
+import multer from "multer";
+// Funções para controlar posts e uploads
+import {
+  listarPosts,
+  postarNovoPost,
+  uploadImagem,
+} from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Salva arquivos em uploads/
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    // Mantém nome original do arquivo
+    cb(null, file.originalname);
+  },
+});
+
+// Configura upload de arquivos
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   // Habilita o middleware `express.json()` para que a aplicação possa entender requisições com corpo   em formato JSON.
   app.use(express.json());
-  // Define uma rota GET para o caminho "/posts".
+  // Busca todos os posts
   app.get("/posts", listarPosts);
+  // Rota para criar um post
+  app.post("/posts", postarNovoPost);
+  // Envia arquivo para uploadImagem
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
